61a50aed44ec6c74b68a8b3c3256c371c11ce354
ryuke <nidhalnaffati@gmail.com>
2023-04-14 14:49:15 +0100

    update the doFilterInternal method.

--- backend/src/main/java/com/nidhal/backend/filter/JwtAuthorizationFilter.java
+++ backend/src/main/java/com/nidhal/backend/filter/JwtAuthorizationFilter.java
@@ -5,6 +5,10 @@ import com.nidhal.backend.entity.User;
 import com.nidhal.backend.service.JwtService;
 import com.nidhal.backend.service.TokenService;
 import com.nidhal.backend.service.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +23,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
 /**
  * This class is a Spring component that extends OncePerRequestFilter, which is a convenient base class for filter
@@ -36,65 +43,89 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     private final UserService userService;
     private final TokenService tokenService;
 
-    /**
-     * Filters incoming requests and authenticates the user associated with a valid JWT in the Authorization header.
-     *
-     * @param request     the HTTP request
-     * @param response    the HTTP response
-     * @param filterChain the filter chain
-     * @throws ServletException if there is an error processing the request
-     * @throws IOException      if there is an I/O error while processing the request
-     */
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String userEmail;
 
-        // Check if Authorization header is missing or does not contain a valid JWT
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (request.getServletPath().contains("/api/v1/register") ||
+            request.getServletPath().contains("/api/v1/auth") ||
+            request.getServletPath().contains("/api/v1/reset-password")
+        ) {
+            log.error("request.getServletPath(): {}", request.getServletPath());
+            log.error("skipping the filter");
             filterChain.doFilter(request, response);
-            return;
-        }
+        } else {
 
-        // Extract the JWT from the Authorization header
-        jwt = authHeader.substring(7);
+            final String authHeader = request.getHeader("Authorization");
+            final String jwt;
+            final String userEmail;
 
-        // Extract the user email from the JWT and check if it is valid
-        userEmail = jwtService.extractUsername(jwt);
+            // Check if Authorization header is missing or does not contain a valid JWT
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                try {
+                    // Extract the JWT from the Authorization header
+                    jwt = authHeader.substring(7);
+
+                    // Extract the user email from the JWT and check if it is valid
+                    userEmail = jwtService.extractUsername(jwt);
 
 
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            // Retrieve the userDetails from the database
-            User user = userService.findUserByEmail(userEmail);
+                    if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                        // Retrieve the userDetails from the database
+                        User user = userService.findUserByEmail(userEmail);
 
-            var isTokenValid = tokenService.isTokenValid(jwt);
-            // Check if the JWT is valid for the retrieved userDetails
+                        var isTokenValid = tokenService.isTokenValid(jwt);
+                        // Check if the JWT is valid for the retrieved userDetails
+                        log.info("isTokenValid: {}", isTokenValid);
 
-            if (jwtService.isTokenValid(jwt, user) && isTokenValid) {
-                // create a new authentication token with the retrieved user
-                log.warn("User {} is authenticated with authorities {}", user.getUsername(), user.getAuthorities());
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        user,
-                        null,
-                        user.getAuthorities()
-                );
+                        if (jwtService.isTokenValid(jwt, user) && isTokenValid) {
+                            // create a new authentication token with the retrieved user
+                            log.warn("User {} is authenticated with authorities {}", user.getUsername(), user.getAuthorities());
+                            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                                    user,
+                                    null,
+                                    user.getAuthorities()
+                            );
 
-                // Set the details of the authentication token
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
+                            // Set the details of the authentication token
+                            authToken.setDetails(
+                                    new WebAuthenticationDetailsSource().buildDetails(request)
+                            );
 
-                // Set the authentication token in the SecurityContextHolder
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+                            // Set the authentication token in the SecurityContextHolder
+                            SecurityContextHolder.getContext().setAuthentication(authToken);
+                        }
+                    }
+                    // Proceed with the filter chain
+                    filterChain.doFilter(request, response);
+                } catch (ExpiredJwtException ex) {
+                    log.warn("JWT has expired: {}", ex.getMessage());
+                    response.setStatus(SC_UNAUTHORIZED);
+                    response.getWriter().write("JWT has expired from the filter");
+                } catch (MalformedJwtException ex) {
+                    log.warn("JWT is malformed: {}", ex.getMessage());
+                    response.setStatus(SC_UNAUTHORIZED);
+                    response.getWriter().write("JWT is malformed");
+                } catch (SignatureException ex) {
+                    log.warn("JWT signature is invalid: {}", ex.getMessage());
+                    response.setStatus(SC_UNAUTHORIZED);
+                    response.getWriter().write("JWT signature is invalid");
+                } catch (UnsupportedJwtException exception) {
+                    log.warn("JWT is unsupported: {}", exception.getMessage());
+                    response.setStatus(SC_UNAUTHORIZED);
+                    response.getWriter().write("JWT is unsupported");
+                } catch (Exception ex) {
+                    log.error("Failed to extract username from JWT: {}", ex.getMessage());
+                    response.sendError(SC_INTERNAL_SERVER_ERROR, "Failed to extract username from JWT");
+                }
+            } else {
+                log.error("Authorization header is missing or does not contain a valid JWT");
+                filterChain.doFilter(request, response);
             }
         }
-
-        // Proceed with the filter chain
-        filterChain.doFilter(request, response);
     }
 }
