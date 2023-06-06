266102bf52aa0b7bf7cab43a7868ae8cf3dd2e4f
NidhalNaffati <nidhalnaffati@gmail.com>
2023-06-06 23:24:04 +0100

    Updating the resetPassword & enableUser method to perform the redirect & print out a better msg error.

--- backend/src/main/java/com/nidhal/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/nidhal/backend/controller/AuthenticationController.java
@@ -1,12 +1,18 @@
 package com.nidhal.backend.controller;
 
 
-import com.nidhal.backend.requests.*;
+import com.nidhal.backend.exception.PasswordDontMatchException;
+import com.nidhal.backend.exception.UserNotFoundException;
+import com.nidhal.backend.requests.AuthenticationRequest;
+import com.nidhal.backend.requests.AuthenticationResponse;
+import com.nidhal.backend.requests.RegisterRequest;
 import com.nidhal.backend.service.AuthenticationService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,13 +26,13 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    @PostMapping("/register")
+    @PostMapping("register")
     public ResponseEntity<String> register(
             @Valid @RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authenticationService.registerUser(request));
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping("authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @Valid @RequestBody AuthenticationRequest request
     ) {
@@ -34,13 +40,31 @@ public class AuthenticationController {
         return ResponseEntity.ok(responseToken);
     }
 
-    @PostMapping("/enable-user/{token}")
-    public ResponseEntity<String> enableUser(@PathVariable String token) {
-        authenticationService.enableUser(token);
-        return ResponseEntity.ok("Account enabled successfully");
+    @PostMapping("enable-user/{token}")
+    public ResponseEntity<String> enableUser(
+            @PathVariable String token
+    ) {
+        try {
+            authenticationService.enableUser(token);
+            // If the enableUser method succeeds, perform the redirect
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .header("Location", "http://localhost:8080/login")
+                    .body(null);
+        } catch (ExpiredJwtException e) { // ExpiredJwtException is a custom exception
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Link has expired. Please request a new one.");
+        } catch (UserNotFoundException e) { // UserNotFoundException is a custom exception
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("User not found !!");
+        } catch (Exception e) { // For any other unhandled exceptions, return a generic error message
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to enable user.");
+        }
     }
 
-    @PostMapping("/refresh-token")
+    @PostMapping("refresh-token")
     public ResponseEntity<AuthenticationResponse> refreshToken(
             HttpServletRequest request,
             HttpServletResponse response
@@ -50,20 +74,35 @@ public class AuthenticationController {
     }
 
 
-    @PostMapping("/reset-password")
+    @PostMapping("reset-password/{token}")
     public ResponseEntity<String> resetPassword(
-            @Valid @RequestBody ResetPasswordRequest request
-    ) {
-        return ResponseEntity.ok(authenticationService.sendResetPasswordRequestToUser(request.email()));
-    }
-
-    @PostMapping("/reset-password/{token}")
-    public ResponseEntity<String> updatePassword(
             @PathVariable String token,
-            @Valid @RequestBody UpdatePasswordRequest request
+            @RequestParam String password,
+            @RequestParam String passwordConfirm
     ) {
-        authenticationService.upDatePassword(token, request.password(), request.passwordConfirm());
-        return ResponseEntity.ok("Password reset successfully");
+        try {
+            authenticationService.upDatePassword(token, password, passwordConfirm);
+            // If the upDatePassword method succeeds, perform the redirect
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .header("Location", "http://localhost:8080/login")
+                    .body(null);
+        } catch (PasswordDontMatchException e) { // PasswordDontMatchException is a custom exception
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Password don't match !!");
+        } catch (ExpiredJwtException e) { // ExpiredJwtException is a custom exception
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Link has expired. Please request a new one.");
+        } catch (UserNotFoundException e) { // UserNotFoundException is a custom exception
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("User not found !!");
+        } catch (Exception e) {  // For any other unhandled exceptions, return a generic error message
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to reset password try again !!");
+        }
     }
 
 }
