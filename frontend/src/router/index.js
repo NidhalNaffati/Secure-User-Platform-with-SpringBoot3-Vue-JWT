901d6e6822b20d91997528bcb4d9272792486702
NidhalNaffati <nidhalnaffati@gmail.com>
2023-06-23 12:07:53 +0100

    feat: Added authentication and authorization guards to router configuration.
    Explanation: The new version of the file includes additional functionality related to authentication and authorization. Specifically, the following changes have been made:
    1. Added `beforeEnter` guards to the routes for "signup", "login", and "forgotten-password" components. These guards check if the user is already authenticated and redirect them to the home page if they are.
    2. Added an `adminGuard` function as a `beforeEnter` guard for the "admin" route. This guard checks if the user is authenticated and also if the user has admin privileges. If the user doesn't meet both criteria, they are redirected to the home page and an error message is logged to the console.
    3. Added a TODO comment to remind developers to implement a message to inform the user that they are not authorized to access the admin page. A suggestion is provided to use the toast component for displaying the message.
    
    These changes enhance the security and user experience of the application by restricting access to certain pages based on authentication and authorization.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import Login from "../views/Login.vue";
 import NotFoundView from "../views/NotFoundView.vue";
 import ForgottenPasswordView from "@/views/ForgottenPasswordView.vue";
 import AdminPage from "@/views/AdminPage.vue";
+import {useAuthStore} from "@/stores";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,21 +19,25 @@ const router = createRouter({
             path: "/signup",
             name: "signup",
             component: Register,
+            beforeEnter: redirectIfAuthenticated,
         },
         {
             path: "/login",
             name: "login",
             component: Login,
+            beforeEnter: redirectIfAuthenticated,
         },
         {
             path: "/forgotten-password",
             name: "forgotten-password",
             component: ForgottenPasswordView,
+            beforeEnter: redirectIfAuthenticated,
         },
         {
             path: "/admin",
             name: "admin",
             component: AdminPage,
+            beforeEnter: adminGuard,
         },
         {
             path: "/404",
@@ -46,5 +51,31 @@ const router = createRouter({
         },
     ],
 });
+
+
+// this function is used to prevent the user from accessing the login, signup and ForgottenPassword pages when he is already authenticated.
+function redirectIfAuthenticated(to, from, next) {
+    const authStore = useAuthStore();
+    if (authStore.isUserAuthenticated) { // If the user is already authenticated
+        next({name: "home"}); // Redirect to home page
+    } else { // Otherwise
+        next(); // Proceed to the requested page
+    }
+}
+
+function adminGuard(to, from, next) {
+    const authStore = useAuthStore();
+    if (authStore.isUserAuthenticated && authStore.isAdmin) {
+        next();
+    } else {
+        next({ name: "home" }); // Redirect to home page
+        console.error("You are not authorized to access this page.");
+        /*
+        * TODO : add a message to the user to tell him that he is not authorized to access this page.
+        *  WE CAN USE THE TOAST COMPONENT FOR THIS
+        * 
+        * */
+    }
+}
 
 export default router
